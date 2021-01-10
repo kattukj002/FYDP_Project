52db8076c97fc97e903048a0105b4fc8b991b216
Felix Cheng <felix.kh.cheng@gmail.com>
2021-01-10 16:43:16 -0500

    Simple bouncing balls in alternate environment

---
+++ fydp_unity/Assets/Scripts/Testing/LogDevices.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class LogDevices : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevices(devices);
+
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+        Debug.Log("Done");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
