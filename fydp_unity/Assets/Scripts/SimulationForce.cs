b3dffd89fe7f807ebc5fabc471be27ecadbfa924
Felix Cheng <felix.kh.cheng@gmail.com>
2021-03-29 20:40:18 -0400

    fix

--- fydp_unity/Assets/Scripts/SimulationForce.cs
+++ fydp_unity/Assets/Scripts/SimulationForce.cs
@@ -142,15 +142,15 @@ public class SimulationForce : MonoBehaviour
             new BraceSensorReader(_arduinoPort, _portMutex), 
             TimeSpan.FromMilliseconds(sensorDataRelevanceLifetimeMs));
 
-        // _armModel = new ArmVectorModel(_sensorReadings,
-        //         calibrationValues, 
-        //         useDummyInputs: UseDummyInputs,
-        //         printIntermediateValues: PrintIntermediateValues,
-        //         useLeftControllerAsElbowTracker: UseLeftControllerAsElbowTracker,
-        //         ignoreImu:IgnoreImu,
-        //         FinalTestDisable:FinalTestDisable);
+        _armModel = new ArmVectorModel(_sensorReadings,
+                calibrationValues, 
+                useDummyInputs: UseDummyInputs,
+                printIntermediateValues: PrintIntermediateValues,
+                useLeftControllerAsElbowTracker: UseLeftControllerAsElbowTracker,
+                ignoreImu:IgnoreImu,
+                FinalTestDisable:FinalTestDisable);
 
-        // _armMotionEstimators = new ArmMotionEstimators(Time.fixedDeltaTime);
+        _armMotionEstimators = new ArmMotionEstimators(Time.fixedDeltaTime);
 
         EditorApplication.playModeStateChanged += (PlayModeStateChange state) => {
             if(state == PlayModeStateChange.ExitingPlayMode){
